2a26fa58606edea1cf61c879ba7b022a14888269
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-04-09 23:22:24 +0200

    Hago más pequeño el textArea

--- src/main/java/calculadora/PanelPrincipal.java
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -6,6 +6,9 @@ package calculadora;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -13,10 +16,10 @@ import javax.swing.JTextArea;
  *
  * @author jorge
  */
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements ActionListener {
 
     // Atributos de la clase (privados)
-    private PanelBotones botonera;
+    private PanelBotones botones;
     private JTextArea areaTexto;
     private int tipoOperacion;
 
@@ -29,9 +32,9 @@ public class PanelPrincipal extends JPanel {
     // Se inicializan los componentes gráficos y se colocan en el panel
     private void initComponents() {
         // Creamos el panel de botones
-        botonera = new PanelBotones();
+        botones = new PanelBotones();
         // Creamos el área de texto
-        areaTexto = new JTextArea(10, 50);
+        areaTexto = new JTextArea(5, 50);
         areaTexto.setEditable(false);
         areaTexto.setBackground(Color.white);
 
@@ -39,7 +42,58 @@ public class PanelPrincipal extends JPanel {
         this.setLayout(new BorderLayout());
         // Colocamos la botonera y el área texto
         this.add(areaTexto, BorderLayout.NORTH);
-        this.add(botonera, BorderLayout.SOUTH);
+        this.add(botones, BorderLayout.SOUTH);
 
+        // Hago que todos los botones sean actionlisteners
+        for (JButton boton : this.botones.getgrupoBotones()) {
+            boton.addActionListener(this);
+        }
+
+    }
+
+    // Implemento la interfaz
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Obtengo el objeto que desencadena el evento
+        Object o = e.getSource();
+        
+        // Si es el botón de reseteo, reseteo
+        if (o.equals(this.botones.grupoBotones[15])){
+            areaTexto.setText("");
+            
+        // Si es el boton de operar, opero
+        } else if (o.equals(this.botones.grupoBotones[14])){
+            // Si es una expresion de calculo con dos operandos, opero
+            if (this.areaTexto.getText().matches("[0-9]+[+-/*][0-9]+")){
+                // Dependiendo de la operación opero
+                if (this.areaTexto.getText().contains("+")){
+                    String[] operandos = this.areaTexto.getText().split("\\+");
+                    int operando1 = Integer.parseInt(operandos[0]);
+                    int operando2 = Integer.parseInt(operandos[1]);
+                    areaTexto.setText(String.valueOf(operando1 + operando2));
+                } else if (this.areaTexto.getText().contains("-")){
+                    String[] operandos = this.areaTexto.getText().split("-");
+                    int operando1 = Integer.parseInt(operandos[0]);
+                    int operando2 = Integer.parseInt(operandos[1]);
+                    areaTexto.setText(String.valueOf(operando1 - operando2));
+                } else if (this.areaTexto.getText().contains("*")){
+                    String[] operandos = this.areaTexto.getText().split("\\*");
+                    int operando1 = Integer.parseInt(operandos[0]);
+                    int operando2 = Integer.parseInt(operandos[1]);
+                    areaTexto.setText(String.valueOf(operando1 * operando2));
+                } else {
+                    String[] operandos = this.areaTexto.getText().split("/");
+                    double operando1 = Double.parseDouble(operandos[0]);
+                    double operando2 = Double.parseDouble(operandos[1]);
+                    areaTexto.setText(String.valueOf(operando1 / operando2));
+                }
+            
+            } // Si no es una expresion de calculo con dos operandos lo dejo igual
+            
+        // Si es otro botón lo escribo
+        } else if (o instanceof JButton) {
+            areaTexto.setText(this.areaTexto.getText() + ((JButton) o).getText());
+        }
+        
     }
 }
