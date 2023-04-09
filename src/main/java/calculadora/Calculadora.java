40bed7dd956cd755f6d05f562a45a8275afaf335
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-04-09 22:07:13 +0200

    Creo la clase calculadora para el main

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author jorge
+ */
+public class Calculadora {
+    
+    // Main de la calculadora
+    public static void main(String[] args) {
+        
+    }
+    
+}
