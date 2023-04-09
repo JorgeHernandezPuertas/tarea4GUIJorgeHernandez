a2657db7ca1388c76170094350a7813b9636dfe0
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-04-09 22:14:29 +0200

    Creo la ventana de la calculadora en el main

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -4,6 +4,8 @@
  */
 package calculadora;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author jorge
@@ -12,6 +14,18 @@ public class Calculadora {
     
     // Main de la calculadora
     public static void main(String[] args) {
+        
+        // Creo la ventana d ela calculadora
+        JFrame ventana = new JFrame("Calculadora");
+    	ventana.setBounds(250, 250, 0, 0);
+        // Le añado el panel principal que he creado
+    	ventana.add(new PanelPrincipal());
+   	 
+        // Le doy visibilidad, el boton de salida y hago un pack para que tenga el
+        // tamaño justo
+    	ventana.setVisible(true);
+    	ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	ventana.pack();
         
     }
     
